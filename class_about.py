b620803e6e18e2799ed0e6ad44111ad0415cff24
M.U <rustamovmuhammadumar2004@gmail.com>
2026-05-12 16:02:33 +0900

    feat: we learnt python special/magic methods today

--- class_about.py
+++ class_about.py
@@ -48,3 +48,47 @@ print(new_message)
 
 # static method
 Person.class_method()  # class method
+
+print("=======special methods========")
+# special methods >> __init__, __str__, __len__, __new__, __getitem__, __eq__, etc
+
+
+class Car():
+    #  state
+    description = "This is a car class"
+    # constructor
+
+    def __new__(cls, *args, **kwargs):
+        print("**__new__ method is called**")
+        return super().__new__(cls)
+
+    def __init__(self, model, year):
+        self.model = model
+        self.year = year
+    # method
+
+    def start_engine(self):
+        print(f"{self.model} engine started")
+
+    def stop_engine(self):
+        print(f"{self.model} engine stopped")
+
+    def __str__(self):
+        return f"{self.model} is produced in ({self.year})"
+
+    def __call__(self):
+        print(f"{self.model} is called as a function")
+        return True
+
+
+my_car = Car("Toyota Camry", 2020)
+my_car.start_engine()
+my_car.stop_engine()
+
+
+print("============")
+your_car = Car("Honda Accord", 2019)
+print(your_car)
+your_car()
+result = your_car()
+print(f"Result of calling your_car as a function is {result}")
